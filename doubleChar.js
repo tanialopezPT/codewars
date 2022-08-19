7c0bd27b0d11fe2286ce7c818ea4d7d9743b81ce
tanialopezPT <taniadag5@gmail.com>
2022-08-18 22:12:39 -0700

    double character 8 kyu

---
+++ doubleChar.js
@@ -0,0 +1,8 @@
+
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+function doubleChar(str) {
+    return str.split('').map( c=> c+c).join("");
+  }
+  
+  
