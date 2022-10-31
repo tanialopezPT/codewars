daf97c99352dfbf21cabfc94051a26c78f993a4e
tanialopezPT <taniadag5@gmail.com>
2022-10-30 22:44:39 -0700

    is this a triangle

--- 7kyu.js
+++ 7kyu.js
@@ -16,3 +16,15 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
     return Math.floor(Math.sqrt(array.map(a=> a*a).reduce((b,c) => b+c)) /2)
     }
   
+
+  
+  
+    // Implement a function that accepts 3 integer values a, b, c. The function should return true if a triangle can be built with the sides of given length and false in any other case.
+
+    // (In this case, all triangles must have surface greater than 0 to be accepted).
+    
+  
+  
+    function isTriangle(a,b,c)
+{
+   return a+b>c && c+b >a && a+c > b
