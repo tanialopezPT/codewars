f31b6060488c81f2bd9c24ff033682da1e3a6641
tanialopezPT <taniadag5@gmail.com>
2022-10-30 22:57:47 -0700

    x&o

--- 7kyu.js
+++ 7kyu.js
@@ -27,4 +27,31 @@ function predictAge(age1,age2,age3,age4,age5,age6,age7,age8){
   
     function isTriangle(a,b,c)
 {
-   return a+b>c && c+b >a && a+c > b
+   return a+b>c && c+b >a && a+c > b}
+
+
+   
+//    Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+   
+   function XO(str) {
+    
+    let x= str.split("").includes("x");
+    let o= str. split("").includes("o");
+    
+    if (x === o) {return true;}
+    else {return false;}
+    
+
+        unction XO(str) {
+        return str.toLowerCase().split('x').length === str.toLowerCase().split('o').length;
+    }
+
+  }
